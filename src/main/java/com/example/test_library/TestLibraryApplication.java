02c1ce09c63a14e70cd6480761f5b9d9a9fdcd56
Oleg Chumin <tschumin.oleg@gmail.com>
2023-04-06 22:53:06 +0300

    Initial commit

---
+++ src/main/java/com/example/test_library/TestLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestLibraryApplication.class, args);
+    }
+
+}
